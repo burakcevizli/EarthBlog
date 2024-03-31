4319e45242e8d24e5c7727eb3a28f3c8cf146197
KrieqqeR <burakcevizli@gmail.com>
2024-03-31 17:18:54 +0300

    User Update and remove with roles .

--- EarthBlog.Web/Areas/Admin/Controllers/UserController.cs
+++ EarthBlog.Web/Areas/Admin/Controllers/UserController.cs
@@ -19,21 +19,21 @@ namespace EarthBlog.Web.Areas.Admin.Controllers
 		private readonly IMapper mapper;
 		private readonly IToastNotification toastNotification;
 
-		public UserController(UserManager<AppUser> userManager,RoleManager<AppRole> roleManager, IMapper mapper,IToastNotification toastNotification)
-        {
+		public UserController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager, IMapper mapper, IToastNotification toastNotification)
+		{
 			this.userManager = userManager;
 			this.roleManager = roleManager;
 			this.mapper = mapper;
 			this.toastNotification = toastNotification;
 		}
-        public async Task<IActionResult> Index()
+		public async Task<IActionResult> Index()
 		{
 			var users = await userManager.Users.ToListAsync();
 			var map = mapper.Map<List<UserDto>>(users);
-			foreach(var item in map)
+			foreach (var item in map)
 			{
 				var findUser = await userManager.FindByIdAsync(item.Id.ToString());
-				var role =string.Join("",await userManager.GetRolesAsync(findUser));
+				var role = string.Join("", await userManager.GetRolesAsync(findUser));
 
 				item.Role = role;
 			}
@@ -51,11 +51,11 @@ namespace EarthBlog.Web.Areas.Admin.Controllers
 			var map = mapper.Map<AppUser>(userAddDto);
 			var roles = await roleManager.Roles.ToListAsync();
 
-			if(ModelState.IsValid)
+			if (ModelState.IsValid)
 			{
 				map.UserName = userAddDto.Email;
 				var result = await userManager.CreateAsync(map, string.IsNullOrEmpty(userAddDto.Password) ? "" : userAddDto.Password);
-				if(result.Succeeded)
+				if (result.Succeeded)
 				{
 					var findRole = await roleManager.FindByIdAsync(userAddDto.RoleId.ToString());
 					await userManager.AddToRoleAsync(map, findRole.ToString());
@@ -64,13 +64,75 @@ namespace EarthBlog.Web.Areas.Admin.Controllers
 				}
 				else
 				{
-					foreach(var errors in result.Errors)
-						ModelState.AddModelError("",errors.Description);
+					foreach (var errors in result.Errors)
+						ModelState.AddModelError("", errors.Description);
 					return View(new UserAddDto { Roles = roles });
 
 				}
 			}
 			return View(new UserAddDto { Roles = roles });
+		}
+		[HttpGet]
+		public async Task<IActionResult> Update(Guid userId)
+		{
+			var user = await userManager.FindByIdAsync(userId.ToString());
+			var roles = await roleManager.Roles.ToListAsync();
+
+			var map = mapper.Map<UserUpdateDto>(user);
+			map.Roles = roles;
+
+			return View(map);
+		}
+		[HttpPost]
+		public async Task<IActionResult> Update(UserUpdateDto userUpdateDto)
+		{
+			var user = await userManager.FindByIdAsync(userUpdateDto.Id.ToString());
+			if (user != null)
+			{
+				var userRole = string.Join("", await userManager.GetRolesAsync(user));
+				var roles = await roleManager.Roles.ToListAsync();
+				if (ModelState.IsValid)
+				{
+					mapper.Map(userUpdateDto, user);
+					user.UserName = userUpdateDto.Email;
+					user.SecurityStamp = Guid.NewGuid().ToString();
+					var result = await userManager.UpdateAsync(user);
+					if (result.Succeeded)
+					{
+						await userManager.RemoveFromRoleAsync(user, userRole);
+						var findRole = await roleManager.FindByIdAsync(userUpdateDto.RoleId.ToString());
+						await userManager.AddToRoleAsync(user, findRole.Name);
+						toastNotification.AddSuccessToastMessage(Messages.User.Update(userUpdateDto.Email), new ToastrOptions { Title = "İşlem Başarılı" });
+						return RedirectToAction("Index", "User", new { Area = "Admin" });
+					}
+					else
+					{
+						foreach (var errors in result.Errors)
+							ModelState.AddModelError("", errors.Description);
+						return View(new UserUpdateDto { Roles = roles });
+
+					}
+				}
+			}
+			return NotFound();
+		}
+		public async Task<IActionResult> Delete(Guid userId)
+		{
+			var user = await userManager.FindByIdAsync(userId.ToString());
+
+			var result = await userManager.DeleteAsync(user);
+
+			if (result.Succeeded)
+			{
+				toastNotification.AddSuccessToastMessage(Messages.User.Delete(user.Email), new ToastrOptions { Title = "İşlem Başarılı" });
+				return RedirectToAction("Index", "User", new { Area = "Admin" });
+			}
+			else
+			{
+				foreach (var errors in result.Errors)
+					ModelState.AddModelError("", errors.Description);
+			}
+			return NotFound();
 		}
 	}
 }
